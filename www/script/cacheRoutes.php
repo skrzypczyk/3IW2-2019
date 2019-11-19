528825a2fe2ef10fddd4ba76726e31f6ebe77ccd
Yves skrzypczyk <skrzypczyk@MacBook-Pro-de-Yves.local>
2019-11-19 12:59:41 +0100

    init solution de cache

--- www/script/cacheRoutes.php
+++ www/script/cacheRoutes.php
@@ -1,5 +1,10 @@
 <?php
 //Récupérer le fichier yaml
-
+$listOfRoutes = yaml_parse_file("../routes.yml");
 //Créer un fichier php dans le dossier cache
 //Contenant toutes les routes sous forme d'array
+//Le fichier devra s'appeler "routes.cache.php"
+
+$data = var_export($listOfRoutes, true);
+
+file_put_contents("../cache/routes.cache.php", "<?php ".$data);
